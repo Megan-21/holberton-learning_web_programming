32adef0ddbf1796384b2c1977069fa3192784267
Megan <boxedwater26@gmail.com>
2019-07-12 10:47:05 -0700

    Update 101-function.js

--- 5-javascript/101-function.js
+++ 5-javascript/101-function.js
@@ -1,3 +1,10 @@
-function showMessage(){
-  console.log("Hello world!");
+var b = myFunction("Hello World!")
+function myFunction(b) {
+  console.log(b)
+}
+
+var a = youFunction(prompt("Give me a number"))
+function youFunction(a) {
+let c = a*2
+  console.log(c);
 }
