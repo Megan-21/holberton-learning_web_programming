7f132190854c28f89b11ebfc4b7ae3a190143fd7
Megan <boxedwater26@gmail.com>
2019-07-15 10:45:33 -0700

    Update 4-clock.js

--- 6-javascript_continued/4-clock.js
+++ 6-javascript_continued/4-clock.js
@@ -6,17 +6,17 @@ function clock(){
   s++;
   if(s==60){
     s=0;
-    x++
-  } if (x==60) {
-    x=0;
-    i++;
-  } if(h==24){
-      h=0;
-    }
+  }
 }
 
 while (i <= 23) {
   console.log(i);
+  if (x=59){
+    i++;
+  }
 } while (x <= 59) {
   console.log(x);
+  if (s=60){
+    x++;
+  }
 }
