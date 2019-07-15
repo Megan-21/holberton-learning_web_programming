5160d5a681a4b7d7b850a835491a048d35b503a2
Megan <boxedwater26@gmail.com>
2019-07-15 10:11:18 -0700

    Update 1-condition.js

--- 6-javascript_continued/1-condition.js
+++ 6-javascript_continued/1-condition.js
@@ -1,5 +1,7 @@
-if (x > 5){
+let my_parameter = process.argv[2];
+
+if (my_parameter > 5){
   console.log("The value is greater than 5");
-} else if (x < 5){
-  console.log();
+} else if (my_parameter < 5){
+  
 }
