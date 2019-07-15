07ed59c2c3bd61a8201919166c4abe6747822d33
Megan <boxedwater26@gmail.com>
2019-07-15 10:37:09 -0700

    Create 4-clock.js

---
+++ 6-javascript_continued/4-clock.js
@@ -0,0 +1,21 @@
+var i = 0
+var x = 0
+
+function clock(){
+  s++;
+  if(s==60){
+    s=0;
+    x++
+  } if (x==60) {
+    x=0;
+    i++;
+  } if(h==24){
+      h=0;
+    }
+}
+
+while (i <= 23) {
+  console.log(i);
+} while (x <= 59) {
+
+}
